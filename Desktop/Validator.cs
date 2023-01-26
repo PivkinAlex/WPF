9568607007c4b692fb8c0161c7f8a1f24d28fdf5
PivkinAlex <alesha.pivkin@mail.ru>
2023-01-26 22:41:36 +0300

    1commit 2lab

---
+++ Desktop/Validator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop
+{
+    public class Validator
+    {
+        public static string PassCheck(string pass)
+        {
+            if (pass.Length >= 7)
+            { 
+                return null; 
+            }
+            else
+            {
+                return "Error";
+            }
+        }
+        public static string LoginCheck(string emailaddress)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(emailaddress);
+
+                return null;
+            }
+            catch (FormatException)
+            {
+                return "Error";
+            }
+        }
+    }
+}
